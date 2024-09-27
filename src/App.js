d688b21cbba532a8aceb025a5a9a1f4dc874afc4
Andreas Giese <andreas.giese@gmail.com>
2024-09-27 09:08:08 +0200

    add menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
+
 import '@aws-amplify/ui-react/styles.css';
-import { withAuthenticator, Button, Heading, Image, View, Card } from '@aws-amplify/ui-react';
+import './App.css';
+import { withAuthenticator, View, Card, Flex, Menu , MenuItem } from '@aws-amplify/ui-react';
 
 function App({ signOut }) {
   return (
     <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={ 1 }>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign out</Button>
+      <Flex direction={'row'} alignItems={'center'} gap={'10px'}>
+        <Card>
+          <Menu>
+            <MenuItem onClick={signOut}>Sign out</MenuItem>
+          </Menu>
+        </Card>
+      </Flex>
     </View>
   );
 }
